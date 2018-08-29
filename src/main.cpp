d18244fc79e905063ac2397cfbe4628d69b303e5
256Michael <256Michal@gmail.com>
2018-08-29 11:34:25 +0200

    Remove useless coment

--- src/main.cpp
+++ src/main.cpp
@@ -22,29 +22,6 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 int main (int argc, char *argv[])
 {
-// 	QSharedMemory pamiec( "notepad-shared-memory" );
-//     if( pamiec.create( 100000000 ) )
-//     {
         NotepadApp app(argc, argv);
-//         if (files != nullptr)
-//             window = new MainWindow(&pamiec, files);
-//         else window = new MainWindow(&pamiec);
         return app.exec();
-//     }
-//     else if( pamiec.attach() )
-//     {
-//         printf( "Podlaczono sie do istniejacego bloku pamieci. Rozmiar bloku wynosi %d.\n", pamiec.size());
-// 
-//         pamiec.lock();
-//         auto* w = (int*) pamiec.data();
-//         std::cin >> *w;
-//         pamiec.unlock();
-//         return 0;
-//     } 
-//     else
-//     {
-//         printf( "Nie udalo sie utworzyc bloku pamieci wspoldzielonej ani sie do niej podlaczyc.\n" );
-//         std::cout <<"Tresc bledu: " << pamiec.errorString().toStdString();
-//         return 1;
-//     }
 }
